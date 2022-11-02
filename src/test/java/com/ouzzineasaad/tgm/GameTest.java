b9088e845ded0780ad5f1f536d211c6f89f1187d
ouzzineasaad <ouzzineasaad@gmail.com>
2022-11-02 02:51:47 +0100

    formatage

--- src/test/java/com/ouzzineasaad/tgm/GameTest.java
+++ src/test/java/com/ouzzineasaad/tgm/GameTest.java
@@ -19,49 +19,48 @@ import main.java.com.ouzzineasaad.tgm.Game;
 import main.java.com.ouzzineasaad.tgm.Score;
 
 public class GameTest {
-	
+
 	@InjectMocks
 	@Spy
 	private Game game = new Game();
-	
+
 	@InjectMocks
 	private Score score;
 
 	@Test
 	public void startTest() throws Exception {
-		
+
 		Mockito.doReturn(true).when(game).isEnded();
 
 		String userInput = String.format("1", System.lineSeparator());
 		ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
 		System.setIn(bais);
 
-	    String expected = "Game Score: 15 - 0";
-	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	    PrintStream printStream = new PrintStream(baos);
-	    System.setOut(printStream);
-	    
-	    game.start(); 
-	    
-	    String[] lines = baos.toString().split(System.lineSeparator());
-	    String actual = lines[lines.length-2];
-	    
+		String expected = "Game Score: 15 - 0";
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(baos);
+		System.setOut(printStream);
 
-	    // checkout output
-	    Assertions.assertEquals(expected,actual);
+		game.start();
+
+		String[] lines = baos.toString().split(System.lineSeparator());
+		String actual = lines[lines.length - 2];
+
+		// checkout output
+		Assertions.assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	void isThereAWinnerTest() throws NoSuchFieldException, SecurityException {
-		Integer[] setScore = new Integer[] {6, 0};
+		Integer[] setScore = new Integer[] { 6, 0 };
 		FieldSetter fieldSetter = new FieldSetter(score, Score.class.getDeclaredField("setScore"));
 		fieldSetter.set(setScore);
 		assertEquals(game.isEnded(), true);
 	}
-	
+
 	@BeforeEach
-	public void setup() throws Exception {	    
-	    MockitoAnnotations.initMocks(this);
+	public void setup() throws Exception {
+		MockitoAnnotations.initMocks(this);
 	}
-	
+
 }
